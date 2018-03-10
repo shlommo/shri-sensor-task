68afa31b9f2bc1c6d9d1b6fc8c4724e05431b02f
event <event@95.local>
2018-03-10 10:46:50 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.set('port', 4000);
+
+app.use(express.static(path.join(__dirname, 'static')));
+
+const server = app.listen(app.get('port'), function() {
+  const port = server.address().port;
+  console.log('Magic happens on port ' + port);
+});
